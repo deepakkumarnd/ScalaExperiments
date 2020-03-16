6a0d45e26a911843609562533e79580ae9225863
Deepak Kumar <deepakkumar@gmail.com>
2020-03-16 15:11:46 +0530

    formatting

--- src/main/scala/euler/problem4.scala
+++ src/main/scala/euler/problem4.scala
@@ -3,19 +3,22 @@ package euler
 import scala.annotation.tailrec
 
 object problem4 extends App {
-/*
+  /*
 
   A palindromic number reads the same both ways. The largest palindrome made from the
   product of two 2-digit numbers is 9009 = 91 × 99.
 
   Find the largest palindrome made from the product of two 3-digit numbers.
-*/
+   */
 
   @tailrec
-  def power(base: Int, pow: Int, acc: Int = 1): Int = if (pow > 0) power(base, pow - 1, acc * base) else acc
+  def power(base: Int, pow: Int, acc: Int = 1): Int =
+    if (pow > 0) power(base, pow - 1, acc * base) else acc
 
   def reverse(num: Int): Int = {
-    def loop(n: Int, acc: Int): Int = if (n > 0) loop(n/10, (acc * 10) + (n%10)) else acc
+    @tailrec
+    def loop(n: Int, acc: Int): Int =
+      if (n > 0) loop(n / 10, (acc * 10) + (n % 10)) else acc
 
     loop(num, 0)
   }
@@ -34,7 +37,7 @@ object problem4 extends App {
       val nDigitFactors = factors.filter(p => (min to max).contains(p))
 
       nDigitFactors
-        .flatMap(p => nDigitFactors.map( q => (p,q)))
+        .flatMap(p => nDigitFactors.map(q => (p, q)))
         .map(p => p._1 * p._2)
         .contains(num)
     }
