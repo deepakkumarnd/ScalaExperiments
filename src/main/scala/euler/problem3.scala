85aaa0b886a3ac1fc667fc4ed6f7805a5e4d5937
Deepak Kumar <deepakkumar@gmail.com>
2020-03-16 12:47:33 +0530

    Format code

--- src/main/scala/euler/problem3.scala
+++ src/main/scala/euler/problem3.scala
@@ -3,10 +3,10 @@ package euler
 import scala.annotation.tailrec
 
 object problem3 extends App {
-/*
+  /*
   The prime factors of 13195 are 5, 7, 13 and 29.
   What is the largest prime factor of the number 600851475143 ?
-*/
+   */
   def factorize(num: Long): List[Long] = {
     @tailrec
     def loop(el: Long, acc: List[Long], limit: Long): List[Long] = {
@@ -38,22 +38,22 @@ object problem3 extends App {
 
   def primeFactors(num: Long): Seq[Long] = {
 
-    def filterNonPrimes(list: List[Long]): List[Long] = for {
-      el <- list
-      if isPrime(el)
-    } yield el
+    def filterNonPrimes(list: List[Long]): List[Long] =
+      for {
+        el <- list
+        if isPrime(el)
+      } yield el
 
     if (num < 2) List()
     else if (num == 2 || num == 3) List(num)
     else filterNonPrimes(factorize(num)).sorted(Ordering[Long].reverse)
   }
 
-
   // val factors = primeFactors(600851475143L)
   val factors = primeFactors(600851475143L)
 
   factors match {
     case List() => println("No prime factors found")
-    case _ => println(s"Highest prime factor is ${factors.head}") // 6857
+    case _      => println(s"Highest prime factor is ${factors.head}") // 6857
   }
 }
