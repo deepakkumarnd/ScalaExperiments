7dac18b8fa738cb2d7114124fd8eee99cecefca1
Deepak Kumar <deepakkumar@gmail.com>
2020-03-22 15:39:31 +0530

    Implementing block

--- src/main/scala/example/Block.scala
+++ src/main/scala/example/Block.scala
@@ -1,17 +1,11 @@
 package example
 
-class Action {
-
-  def apply(fn: () => Unit): Unit = {
-    println("Running block")
-    fn()
-  }
-}
-
 object Block extends App {
-
-  val myAction = new Action() {
-    println("My action")
+  def Action(fn: => Unit) = {
+    fn
   }
 
+  Action {
+    println("Hi this is a block")
+  }
 }
