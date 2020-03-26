aa8ac1a584e61dbdc88e16d84bb30c78c19a8efc
Deepak Kumar <deepakkumar@gmail.com>
2020-03-26 13:07:59 +0530

    Password security

--- build.sbt
+++ build.sbt
@@ -2,8 +2,8 @@ import Dependencies._
 
 ThisBuild / scalaVersion     := "2.12.10"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.example"
-ThisBuild / organizationName := "example"
+ThisBuild / organization     := "io.github.basicobject"
+ThisBuild / organizationName := "BasicObject"
 
 lazy val cassandraDependencies = Seq(
   "com.datastax.oss" % "java-driver-core" % "4.5.0",
---
+++ src/main/scala/example/Security.scala
@@ -0,0 +1,49 @@
+package example
+
+// Credits: 
+// https://medium.com/@kasunpdh/how-to-store-passwords-securely-with-pbkdf2-204487f14e84
+// https://alvinalexander.com/source-code/scala-how-to-convert-array-bytes-to-hex-string/
+
+package util
+
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+import java.security.NoSuchAlgorithmException
+import java.security.spec.InvalidKeySpecException
+import java.io.UnsupportedEncodingException
+import java.util.UUID
+
+object Security extends App {
+  
+  def hashedPassword(password: String, salt: String): String = {
+    val iterations = 10000
+    val keyLength = 512
+    val passwordChars = password.toCharArray
+    val saltBytes = salt.getBytes
+    val hashedBytes = hashPassword(passwordChars, saltBytes, iterations, keyLength)
+    convertBytesToHex(hashedBytes)
+  }
+
+  def hashPassword(password: Array[Char], salt: Array[Byte], iterations: Int, keyLength: Int): Array[Byte] = try {
+    val skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+    val spec = new PBEKeySpec(password, salt, iterations, keyLength)
+    val key = skf.generateSecret(spec)
+    val res = key.getEncoded
+    res
+  } catch {
+    case e@(_: NoSuchAlgorithmException | _: InvalidKeySpecException) =>
+      throw new RuntimeException(e)
+  }
+
+  def convertBytesToHex(bytes: Seq[Byte]): String = {
+    val sb = new StringBuilder
+    for (b <- bytes) {
+        sb.append(String.format("%02x", Byte.box(b)))
+    }
+    sb.toString
+  }
+
+  // val mysalt = UUID.randomUUID.toString
+  val mysalt = "mysalt"
+  println(Security.hashedPassword("mypassword", mysalt))
+}
